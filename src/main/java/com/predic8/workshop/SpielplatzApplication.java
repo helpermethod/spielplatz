907d6010d0aa1963c2f3dd522abe10b9aeb34d1d
helpermethod <weiler@predic8.de>
2016-10-13 15:33:15 +0200

    New changelist

--- src/main/java/com/predic8/workshop/SpielplatzApplication.java
+++ src/main/java/com/predic8/workshop/SpielplatzApplication.java
@@ -2,12 +2,11 @@ package com.predic8.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
+@EnableOAuth2Sso
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
